2986bf0d8052aba1a573ce226d81d4c854003d35
Tom Brews Views <parandykt@gmail.com>
2023-09-15 00:25:43 +0200

    fix client id

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ const app = express();
 
 app.get('/auth/login', async (req, res) => {
   const authorizationURL = await workos.sso.getAuthorizationURL({
-    clientId: 'client_01HAA4PTCS6PHQM5H3S4QE575C', // Replace with your actual client ID
+    clientID: 'client_01HAA4PTCS6PHQM5H3S4QE575C', // Replace with your actual client ID
     domain: 'placeholder.com',
     redirectURI: 'http://localhost:3000/auth/callback',
   });
