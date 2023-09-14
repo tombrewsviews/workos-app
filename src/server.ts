07a7c3bb41dae7942601ac78e29c1d6b22d95b46
Tom Brews Views <parandykt@gmail.com>
2023-09-15 00:28:16 +0200

    adding correct domain

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ const app = express();
 app.get('/auth/login', async (req, res) => {
   const authorizationURL = await workos.sso.getAuthorizationURL({
     clientID: 'client_01HAA4PTCS6PHQM5H3S4QE575C', // Replace with your actual client ID
-    domain: 'placeholder.com',
+    domain: 'https://workos-9qgdfegea-tomparandyk-neontech.vercel.app',
     redirectURI: 'http://localhost:3000/auth/callback',
   });
 
