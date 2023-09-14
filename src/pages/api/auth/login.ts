220950eb26093497171cc42a706cea220b0df79f
Tom Brews Views <parandykt@gmail.com>
2023-09-15 00:18:36 +0200

    fix the variable in logi

--- src/pages/api/auth/login.ts
+++ src/pages/api/auth/login.ts
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import workos from '../../../workos';  
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const clientId = req.query.client_id as string;
+  const clientID = req.query.client_id as string;
 
   const authorizationUrl = await workos.sso.getAuthorizationURL({
     clientID,
